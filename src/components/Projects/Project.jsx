c5daec0733ba3df0ad4aecce6631e2aa33b48071
aaishahhamdha <ah20011024@gmail.com>
2024-09-11 16:16:32 +0530

    new changes

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -8,20 +8,52 @@ import port from '../../assets/port.png';
 import price from '../../assets/price.png';
 
 const projects = [
-  { id: 1, imgsrc: hw, name: 'Mactim', description: 'A device to Monitor and Control Theft in MotorBikes', tech: 'Arduino, C, GPS, GSM, IR sensor, Accelerometer, LED display, Atmel Studio, Blender, KiCad' ,tech2: 'Arduino, C, GPS, GSM, IR sensor, Accelerometer, LED display, Atmel Studio, Blender, KiCad', details: 'Contribution: Developed GSM module and integrated IR sensor. Created 3D models using Blender and produced a demonstration video using iMovie.', link: '' },
+  { 
+    id: 1, 
+    imgsrc: hw, 
+    name: 'Mactim', 
+    description: 'A device to Monitor and Control Theft in MotorBikes', 
+    tech: 'Arduino, C, GPS, GSM, IR sensor, Accelerometer, LED display, Atmel Studio, Blender, KiCad', 
+    tech2: 'Arduino, C, GPS, GSM, IR sensor, Accelerometer, LED display, Atmel Studio, Blender, KiCad', 
+    details: 'The Mactim device features an anti-theft mode activated by a secure button press, detecting falls, theft attempts, and unauthorized ignition, sending alerts with GPS coordinates to the owner. In default mode, it monitors speed to alert riders if they exceed safe limits, promoting responsible riding. Key components include an Arduino Mega 2560, GPS, GSM, IR sensor, LCD 1602, MPU6050 accelerometer, vibration sensors, buzzer, and buttons.',
+    contribution: 'Responsible for developing the GSM module for SMS alerts and integrating the IR sensor for speed monitoring.',
+    link: '' 
+  },
   {
     id: 2,
     imgsrc: sw,
     name: 'Dataverse',
     description: 'NLP-based data visualization tool',
-    tech: 'Frontend: React.js | Authentication & Authorization: OAuth| JWT , LLM: OpenAI| LangChain , Backend: Python| Django REST Framework , Database: SQLite , Visualization: Chart.js , API: Django REST Framework (DRF), Postman',
+    tech: 'Frontend: React.js | Authentication & Authorization: OAuth | JWT , LLM: OpenAI | LangChain , Backend: Python | Django REST Framework , Database: SQLite , Visualization: Chart.js , API: Django REST Framework (DRF), Postman',
     tech2: 'React.js , OAuth , OpenAI, LangChain , Django REST Framework , SQLite , Chart.js , Postman',
-    details: 'Key Functionalities: Natural language to SQL query generation, JWT & social login authentication, user management, profile customization, data visualization, database management, collaboration, chat & voice input for queries, admin dashboard.\n My Contributions: UI design in Figma, JWT-based login, social login, role management, profile editing, user & role management, collaboration, database views, natural language to SQL conversion, system module integration, and UI refinement.',
+    details: 'Key Functionalities: Natural language to SQL query generation, JWT & social login authentication, user management, profile customization, data visualization, database management, collaboration, chat & voice input for queries, admin dashboard.',
+    contribution: 'UI design in Figma, JWT-based traditional and social authentication , role management, profile editing, user & role management, collaboration, database views, natural language to SQL conversion, system module integration, and UI refinement.',
     link: ''
   },  
-  { id: 3, imgsrc: price, name: 'PriceRadar', description: 'Webscraper application to scrape amazon products', tech: ' NextJs, NodeJs, MongoDB, Tailwind CSS, Cheerio , Bright Data', tech2: ' NextJs, NodeJs, MongoDB, Tailwind CSS, Cheerio , Bright Data',details: 'Features: Web scraper application that scrapes Amazon products and tracks their prices.', link: '' },
-  { id: 4, imgsrc: port, name: 'Portfolio', description: 'Responsive personal website', tech: 'ReactJs', tech2: 'ReactJs', details: 'A fully responsive personal portfolio website featuring key sections such as Hero, About, Skills, Projects, Services, and Contact. The site includes smooth scrolling navigation for a seamless user experience, customizable dark/light mode options, and is optimized for flawless interaction across all devices.', link: '' },
+  { 
+    id: 3, 
+    imgsrc: price, 
+    name: 'PriceRadar', 
+    description: 'Webscraper application to scrape Amazon products', 
+    tech: ' NextJs, NodeJs, MongoDB, Tailwind CSS, Cheerio , Bright Data', 
+    tech2: ' NextJs, NodeJs, MongoDB, Tailwind CSS, Cheerio , Bright Data',
+    details: 'Features: Web scraper application that scrapes Amazon products and tracks their prices.', 
+    contribution: 'Full devolopment', 
+    link: '' 
+  },
+  { 
+    id: 4, 
+    imgsrc: port, 
+    name: 'Portfolio', 
+    description: 'Responsive personal website', 
+    tech: 'ReactJs', 
+    tech2: 'ReactJs', 
+    details: 'A fully responsive personal portfolio website featuring key sections such as Hero, About, Skills, Projects, Services, and Contact. The site includes smooth scrolling navigation for a seamless user experience, customizable dark/light mode options, and is optimized for flawless interaction across all devices.', 
+    contribution: 'Full devolopment', 
+    link: '' 
+  },
 ];
+
 
 const chunkArray = (array, chunkSize) => {
   const result = [];
@@ -100,6 +132,8 @@ const Project = () => {
               <h3>{activeProject.description}</h3>
               <p><strong>Project Details:</strong></p>
               <p>{activeProject.details}</p>
+              <p><strong>My contribution:</strong></p>
+              <p>{activeProject.contribution}</p>
               <p><strong>Technologies Used:</strong></p>
               <ul>
                 {activeProject.tech.split(', ').map((tech, index) => (
