03b28d061663216f7fe0907e9ede1ec3b96ad54a
aaishahhamdha <ah20011024@gmail.com>
2024-09-11 16:53:45 +0530

    certificate carousel

--- src/components/Contacts/contact.jsx
+++ src/components/Contacts/contact.jsx
@@ -4,45 +4,56 @@ import { Carousel } from 'react-responsive-carousel';
 import './contact.css'; 
 import { FaEnvelope, FaPhoneAlt, FaLinkedin, FaGithub } from 'react-icons/fa';
 
+import certOne from '../../assets/certifs/1.png';
+import certTwo from '../../assets/certifs/2.png';
+import certThree from '../../assets/certifs/3.png';
+import certFour from '../../assets/certifs/4.png';
+import certFive from '../../assets/certifs/5.png';
+import certSix from '../../assets/certifs/10.png';
 
-import one from '../../assets/1.jpg';
-import two from '../../assets/2.jpg';
-import three from '../../assets/3.jpg';
-import four from '../../assets/4.jpg';
-import five from '../../assets/5.jpg';
-import six from '../../assets/6.jpg';
+import certEight from '../../assets/certifs/14.png';
+import certNine from '../../assets/certifs/15.png';
+import certTen from '../../assets/certifs/16.png';
+import certEleven from '../../assets/certifs/9.jpg';
 
 const Contact = () => {
 
   const carousalImgs = [
-    { src: one, alt: 'Image 1' },
-    { src: two, alt: 'Image 2' },
-    { src: three, alt: 'Image 3' },
-    { src: four, alt: 'Image 4' },
-    { src: five, alt: 'Image 5' },
-    { src: six, alt: 'Image 6' },
+    { src: certOne, alt: 'Certification 1' },
+    { src: certTwo, alt: 'Certification 2' },
+    { src: certThree, alt: 'Certification 3' },
+    { src: certFour, alt: 'Certification 4' },
+    { src: certFive, alt: 'Certification 5' },
+    { src: certSix, alt: 'Certification 6' },
+    
+    { src: certEight, alt: 'Certification 8' },
+    { src: certNine, alt: 'Certification 9' },
+    { src: certTen, alt: 'Certification 10' },
+    { src: certEleven, alt: 'Certification 11' }
   ];
 
   return (
     <section className='cont'>
       <div className="contact-container">
       <div className="carousel-wrapper">
-          <Carousel
-            showThumbs={true}
-            autoPlay
-            infiniteLoop
-            interval={2000}
-            showArrows={false}
-            showStatus={false}
-          >
-            {
-              carousalImgs.map((image) => (
-                <div key={image.alt} className="carousel-image-wrapper">
-                  <img src={image.src} alt={image.alt} className="slide-image" />
-                </div>
-              ))
-            }
-          </Carousel>
+      <Carousel
+  showThumbs={true}   // Hide the thumbnail preview
+  autoPlay
+  infiniteLoop
+  interval={2000}
+  showArrows={false}
+  showStatus={false}
+  renderIndicator={() => null} // Override the default indicator rendering
+>
+  {
+    carousalImgs.map((image) => (
+      <div key={image.alt} className="carousel-image-wrapper">
+        <img src={image.src} alt={image.alt} className="slide-image" />
+      </div>
+    ))
+  }
+</Carousel>
+
 
         
         </div>
